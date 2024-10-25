3d62747db5602b852dcd6a034f4c71d667ade2f1
Cristina <Cristina.Cabral@triosstudent.com>
2024-10-24 21:40:10 -0400

    Console log added

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
 alert("This is an alert message!");
+
+function showAlert() {
+  console.log("Alert is about to be triggered."); 
+  alert("Hello! This is a JavaScript alert.");
+  console.log("Alert was triggered successfully."); 
+}
